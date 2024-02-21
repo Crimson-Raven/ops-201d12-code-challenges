463d725344f4ec6dbee6a11160ebbbc9fb4f4703
Crimson-Raven <becks2028@gmail.com>
2024-02-20 21:20:59 -0600

    helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Hello World Script 1
+# Author:                       Becca
+# Date of latest revision:      02/20/2024
+# Purpose:                      To create a line of script
+
+# Declaration of variables
+
+
+echo "hello world"
+
+
+# Main
+
+
+
+# End
