bf261d11dc5b402a6744f708845a341ed25a8eb1
CrimsonRaven <becks2028@gmail.com>
2024-02-29 00:12:34 -0600

    class08.sh

---
+++ class08.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge Class 07
+# Author:                       Becca
+# Date of latest revision:      02/28/2024
+# Purpose:                      
+
+# I will attempt this lab tomorrow
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+
+# Main
+ 
+
+# End
